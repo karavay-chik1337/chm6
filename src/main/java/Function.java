a71d5592816317090fabdc5881f26a5f19f1cef7
Karavaev2024 <dima_karavaev04@mail.ru>
2024-06-04 17:37:08 +0300

    #3

--- src/main/java/Function.java
+++ src/main/java/Function.java
@@ -1,20 +1,20 @@
 import static java.lang.Math.pow;
 
 public class Function {
-    public static double f(double x) {
-        return 3 * pow(x, 3) - 3 * pow(x, 2) * x + 6 * x / x;
+    //u'''(x)=3u-u'-xu'+2u''+1/2 xu''
+    public static double exactU(double x) {
+        return pow(x, 3) - 6 * x;
     }
 
-    public static double trueU(double x) {
-        return pow(x, 3);
+    public static double exactW(double x) {
+        return 3 * pow(x, 2) - 6;
     }
-    public static double trueW(double x) {
-        return 3 * pow(x, 2);
-    }
-    public static double trueZ(double x) {
+
+    public static double exactZ(double x) {
         return 6 * x;
     }
-    public static double fz(double x, double u, double w, double z) {
-        return 3 * u - w * x + z / x;
+
+    public static double f(double x, double u, double w, double z) {
+        return 3 * u - w - w * x + 2 * z + 0.5 * x * z;
     }
 }
--- src/main/java/ShootingMethod.java
+++ src/main/java/ShootingMethod.java
@@ -22,16 +22,16 @@ public class ShootingMethod {
 
     public ShootingMethod() {}
 
-    public ShootingMethod(double a, double b, int N, double eps, int K, double alpha0, double A, double B, double C) {
+    public ShootingMethod(double a, double b, int N, double eps, int K, double alpha0) {
         this.a = a;
         this.b = b;
         this.n = N;
         this.eps = eps;
         this.K = K;
         this.alpha0 = alpha0;
-        this.A = A;
-        this.B = B;
-        this.C = C;
+        this.A = Function.exactU(a);
+        this.B = Function.exactZ(a);
+        this.C = Function.exactW(b);
 
         this.h = (b - a) / n;
 
@@ -41,33 +41,62 @@ public class ShootingMethod {
             x[i] = a + (h * i);
     }
 
-    public void method() {
-        int L = 0;
-        List<Double> alpha = new ArrayList<>();
-        alpha.add(alpha0);
-        int alphaPos = 0;
-
-        List<Double> fi = new ArrayList<>();
-        fi.add(RungeKutt(alpha.get(alphaPos)));
-        alphaPos++;
-
-        //System.out.println("\nSecant method:");
-        while (L < K && fi.getLast() > eps) {
-
-//            alpha.add(alpha.get(alphaPos) - fi.get(alphaPos) * (alpha.get(alphaPos) - alpha.get(alphaPos - 1)) / (fi.get(alphaPos) - fi.get(alphaPos - 1)));
-//            fi.add(RungeKutt(alpha.getLast()));
-            alphaPos++;
+    public void bisectionMethod() {
+        long L = 0;
+        int ier = 0;
+        double delta = 2 * alpha0;
+        double alpha1 = alpha0 + delta;
+        double phi1 = RungeKutt(alpha0);
+        double phi2 = RungeKutt(alpha1);
+        if (phi1 * phi2 > 0) {
+            alpha1 = alpha0 - delta;
+            phi2 = RungeKutt(alpha1);
+        }
+        double alpha = 0;
+        while (L < K && Math.abs(alpha1 - alpha0) > 2 * eps) {
+            alpha = (alpha0 + alpha1) / 2;
+            double phi3 = RungeKutt(alpha);
+            if (phi1 * phi3 < 0) {
+                alpha1 = alpha;
+            } else if (phi2 * phi3 < 0) {
+                alpha0 = alpha;
+            } else if (abs(phi3) < eps) {
+                break;
+            }
+            alpha = (alpha0 + alpha1) / 2;
             L++;
         }
-
-        if (L >= K) {
-            System.out.println("IER: " + 1);
+        if (L == K) {
+            ier = 1;
         }
-        else {
-            System.out.println("IER: " + 0);
-            System.out.println("Iterations: " + L);
-            System.out.println("Final alpha: " + alpha.getLast());
-        }
+        System.out.println("L: " + L + "\nalpha: " + alpha + "\nIER: " + ier);
+//        int L = 0;
+//        List<Double> alpha = new ArrayList<>();
+//        alpha.add(alpha0);
+//        alpha.add(alpha1);
+//        int alphaPos = 0;
+//
+//        List<Double> target = new ArrayList<>();
+//        target.add(RungeKutt(alpha.get(alphaPos)));
+//        alphaPos++;
+//        target.add(RungeKutt(alpha.get(alphaPos)));
+//
+//        System.out.println("\nSecant method:");
+//        while (L < K && target.getLast() > eps) {
+//            alpha.add(alpha.get(alphaPos) - target.get(alphaPos) * (alpha.get(alphaPos) - alpha.get(alphaPos - 1)) / (target.get(alphaPos) - target.get(alphaPos - 1)));
+//            target.add(RungeKutt(alpha.getLast()));
+//            alphaPos++;
+//            L++;
+//        }
+//
+//        if (L >= K) {
+//            System.out.println("IER: " + 1);
+//        }
+//        else {
+//            System.out.println("IER: " + 0);
+//            System.out.println("Iterations: " + L);
+//            System.out.println("Final alpha: " + alpha.getLast());
+//        }
 
     }
 
@@ -83,30 +112,30 @@ public class ShootingMethod {
             if (i == 1) {
                 System.out.printf("%-10s \t %-10s \t %-10s \t %-10s \t %-10s \t %-10s \t %-10s \n", "x", "u", "du", "w", "dw", "z", "dz");
                 System.out.printf("%-10f \t %-10f \t %-10f \t %-10f \t %-10f \t %-10f \t %-10f \n", x[0], u[0],
-                        abs(u[0] - Function.trueU(x[0])), w[0], abs(w[0] - Function.trueW(x[0])), z[0], abs(z[0] - Function.trueZ(x[0])));
+                        abs(u[0] - Function.exactU(x[0])), w[0], abs(w[0] - Function.exactW(x[0])), z[0], abs(z[0] - Function.exactZ(x[0])));
             }
             double k11 = h * w[i-1];
-            double k21 = h * z[i-1];
-            double k31 = h * Function.fz(x[i-1], u[i-1], w[i-1], z[i-1]);
+            double k12 = h * z[i-1];
+            double k13 = h * Function.f(x[i-1], u[i-1], w[i-1], z[i-1]);
 
-            double k12 = h * (w[i-1] + k21 / 2);
-            double k22 = h * (z[i-1] + k31 / 2);
-            double k32 = h * Function.fz(x[i-1] + h / 2, u[i-1] + k11 / 2, w[i-1] + k21 / 2, z[i-1] + k31 / 2);
+            double k21 = h * (w[i-1] + k12 / 2);
+            double k22 = h * (z[i-1] + k13 / 2);
+            double k23 = h * Function.f(x[i-1] + h / 2, u[i-1] + k11 / 2, w[i-1] + k12 / 2, z[i-1] + k13 / 2);
 
-            double k13 = h * (w[i-1] + k22 / 2);
-            double k23 = h * (z[i-1] + k32 / 2);
-            double k33 = h * Function.fz(x[i-1] + h / 2, u[i-1] + k12 / 2, w[i-1] + k22 / 2, z[i-1] + k32 / 2);
+            double k31 = h * (w[i-1] + k22 / 2);
+            double k32 = h * (z[i-1] + k23 / 2);
+            double k33 = h * Function.f(x[i-1] + h / 2, u[i-1] + k21 / 2, w[i-1] + k22 / 2, z[i-1] + k23 / 2);
 
-            double k14 = h * (w[i-1] + k23);
-            double k24 = h * (z[i-1] + k33);
-            double k34 = h * Function.fz(x[i-1] + h, u[i-1] + k13, w[i-1] + k23, z[i-1] + k33);
+            double k41 = h * (w[i-1] + k32);
+            double k42 = h * (z[i-1] + k33);
+            double k43 = h * Function.f(x[i-1] + h, u[i-1] + k31, w[i-1] + k32, z[i-1] + k33);
 
-            u[i] = u[i-1] + (k11 + 2 * k12 + 2 * k13 + k14) / 6;
-            w[i] = w[i-1] + (k21 + 2 * k22 + 2 * k23 + k24) / 6;
-            z[i] = z[i-1] + (k31 + 2 * k32 + 2 * k33 + k34) / 6;
-            System.out.printf("%-10f \t %-10f \t %-10f \t %-10f \t %-10f \t %-10f \t %-10f \n", x[i], u[i], abs(u[i] - Function.trueU(x[i])), w[i], abs(w[i] - Function.trueW(x[i])), z[i], abs(z[i] - Function.trueZ(x[i])));
+            u[i] = u[i-1] + (k11 + 2 * k21 + 2 * k31 + k41) / 6;
+            w[i] = w[i-1] + (k12 + 2 * k22 + 2 * k32 + k42) / 6;
+            z[i] = z[i-1] + (k13 + 2 * k23 + 2 * k33 + k43) / 6;
+            System.out.printf("%-10f \t %-10f \t %-10f \t %-10f \t %-10f \t %-10f \t %-10f \n", x[i], u[i], abs(u[i] - Function.exactU(x[i])), w[i], abs(w[i] - Function.exactW(x[i])), z[i], abs(z[i] - Function.exactZ(x[i])));
         }
 
-        return abs(w[n] - C);
+        return w[n] - C;
     }
 }
